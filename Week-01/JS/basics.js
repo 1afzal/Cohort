8066989fd1e44104b7d27ff04b57612b751714d4
1afzal <afzalali5256@gmail.com>
2024-12-11 11:04:54 +0530

    week01

---
+++ Week-01/JS/basics.js
@@ -0,0 +1,20 @@
+"use strict"; //treats code as newer version of js
+const accountID = 1342 //only for constants
+let accountMail = "afzal@123.com" //mostly used
+var accountPass = "1234" //Isnt used now
+let accountCity = "Mangalore" //no need of declaring type of variable
+let name1; //undefined
+console.log(name1)
+console.log(accountID)
+
+
+//DataTypes -> Number , bigint , string , boolean , null , undefined , symbol , object  
+
+console.log(typeof accountID)
+
+let score = 67
+console.log(typeof(score))
+
+let isLoggedIn = 1
+let booleanStatus = Boolean(isLoggedIn)
+console.log(booleanStatus)
